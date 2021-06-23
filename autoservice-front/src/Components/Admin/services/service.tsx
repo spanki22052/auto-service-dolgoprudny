41d0ace516597492d31cc3986c55d5aa99b024be
spanki22052 <useful22052@gmail.com>
2021-06-24 01:04:24 +0300

    services update element ready

--- autoservice-front/src/Components/Admin/services/service.tsx
+++ autoservice-front/src/Components/Admin/services/service.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Firebase from '../../Firebase';
 import {
   AdminServicesBlock,
@@ -6,37 +6,93 @@ import {
   InputElement,
 } from '../../MainPage/styled';
 
-interface InputsInterface {
+interface ServicesInterface {
   description: string;
   title: string;
   image: string;
-  price: number;
+  price: string;
 }
 
 const ServiceElement = () => {
-  const [InputState, setInputState] = useState<InputsInterface[]>([]);
+  const [InputState, setInputState] = useState<ServicesInterface>({
+    description: '',
+    title: '',
+    image: '',
+    price: '',
+  });
+  const [servicesState, setServicesState] = useState<ServicesInterface[]>([]);
 
-  Firebase.collection('services')
-    .doc('servicesList')
-    .get()
-    .then((el) => {
-      if (el.exists) {
-        const elData = el.data();
-        elData !== undefined
-          ? setInputState(elData.services)
-          : console.log('no doc');
-      } else {
-        console.log('No such document!');
-      }
-    });
+  console.log(InputState);
+
+  useEffect(() => {
+    Firebase.collection('services')
+      .doc('servicesList')
+      .get()
+      .then((el) => {
+        if (el.exists) {
+          const elData = el.data();
+          elData !== undefined
+            ? setServicesState(elData.service)
+            : console.log('no doc');
+        } else {
+          console.log('No such document!');
+        }
+      });
+  }, []);
+
+  const updateServicesList = (updateElement: ServicesInterface) => {
+    Firebase.collection('services')
+      .doc('servicesList')
+      .set({ service: [...servicesState, updateElement] });
+  };
 
   return (
     <AdminServicesBlock>
       <div className="service-block">
-        <form className="nun">
-          <InputElement placeholder="Сервис" />
-          <InputElement placeholder="Комментарий к сервису" />
-          <InputElement placeholder="Ссылка на картинку" />
+        <form
+          onSubmit={(e: React.SyntheticEvent) => {
+            InputState.title.length > 3 &&
+              InputState.description.length > 3 &&
+              InputState.price.length > 1 &&
+              updateServicesList(InputState);
+            setInputState({
+              description: '',
+              title: '',
+              image: '',
+              price: '',
+            });
+            e.preventDefault();
+          }}
+          className="nun"
+        >
+          <InputElement
+            value={InputState.title}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setInputState({ ...InputState, title: e.target.value });
+            }}
+            placeholder="Сервис"
+          />
+          <InputElement
+            value={InputState.description}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setInputState({ ...InputState, description: e.target.value });
+            }}
+            placeholder="Комментарий к сервису"
+          />
+          <InputElement
+            value={InputState.image}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setInputState({ ...InputState, image: e.target.value });
+            }}
+            placeholder="Ссылка на картинку"
+          />
+          <InputElement
+            value={InputState.price}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setInputState({ ...InputState, price: e.target.value });
+            }}
+            placeholder="Цена от"
+          />
           <BlueButton>Добавить</BlueButton>
         </form>
       </div>
